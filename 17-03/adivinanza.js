04048f8797778eb484da49ed01be8e00fbf53281
gerald-77 <geraldwgomez@gmail.com>
2026-03-17 21:05:45 +0000

    tercer commit

---
+++ 17-03/adivinanza.js
@@ -0,0 +1,8 @@
+let a=15 ;
+let b = 15;
+if(a==b)
+    if (a==b){
+    console.log("adivinaste")
+}
+
+
---
+++ 17-03/mayor_menorque.js
@@ -0,0 +1,10 @@
+let a=40;
+let b=40;
+if(a==b){
+    console.log("Adivinaste");
+}else if(a>b){
+    console.log("muy grande");
+}
+else if(a<b){ 
+    console.log("muy pequeño");
+}
