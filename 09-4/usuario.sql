c8b480a5288b44099e500dcf42c81758a1790a21
gerald-77 <geraldwgomez@gmail.com>
2026-04-09 20:00:03 +0000

    sexto commit

---
+++ 09-4/usuario.sql
@@ -0,0 +1,11 @@
+Create database  SMC proyecto;
+Use SMC proyecto;
+Create table Usuario (
+    cedula INT primary key ,
+    nombre Varchar ,
+    apellido Varchar,
+    correo VARCHAR )
+
+    // el primero es INT , por que solo son datos numericos .
+    // y los otros 3 son VARCHAR , por que solo se admiten datos tipo texto.
+
